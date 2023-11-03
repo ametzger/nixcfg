9629d0d700084e818a276ba89f3b4ed5207da2a1
Alex Metzger <asm@asm.io>
2023-11-03 17:44:11 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "asm nix configuration";
+
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ { self, nixpkgs, nix-darwin, home-manager, ... }:
+  let
+    configuration = { pkgs, ... }: {
+      environment.systemPackages =
+        [ pkgs.vim
+          pkgs.nnn
+        ];
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;  # default shell on catalina
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 4;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "x86_64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#asm-mbp-16
+    darwinConfigurations."asm-mbp-16" = nix-darwin.lib.darwinSystem {
+      system = "x86_64-darwin";
+
+      modules = [
+        configuration
+
+        home-manager.darwinModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.extraSpecialArgs = inputs;
+
+          # home-manager.users.asm = import ./home;
+        }
+
+      ];
+    };
+  };
+}
