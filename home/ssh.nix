fe6076384bbb1e608add11227537ae5c3979c5d4
Alex Metzger <asm@asm.io>
2023-11-15 12:29:38 -0500

    ssh: move stuff over from dotfiles repo

--- home/ssh.nix
+++ home/ssh.nix
@@ -8,11 +8,24 @@
       UseKeychain yes
     '';
 
+    includes = [ "~/.ssh/config.private" ];
+
     matchBlocks = {
       "i-*" = {
         proxyCommand = ''
           sh -c "aws ssm start-session --target %h --document-name AWS-StartSSHSession --parameters 'portNumber=%p'"
         '';
+      };
+
+      "util" = {
+        user = "ec2-user";
+        identityFile = "~/.ssh/id_ed25519";
+        checkHostIP = false;
+        extraOptions = {
+          StrictHostKeyChecking = "no";
+          UserKnownHostsFile = "/dev/null";
+        };
+        proxyCommand = "~/bin/util-ssh";
       };
     };
   };
