57bb7fdf3531300e4bccad4c5073aabb9264b874
Alex Metzger <asm@asm.io>
2025-04-23 15:30:32 -0400

    home-manager: update zsh config to use `initContent` instead of `initExtra`

--- home/zsh.nix
+++ home/zsh.nix
@@ -47,58 +47,62 @@
       [[ ! $TERM == "dumb" ]] && TERM=xterm-256color
     '';
 
-    initExtraFirst = ''
-      # Handle dumb terms
-      [[ $TERM == "dumb" ]] && unsetopt zle && PS1='$ ' && return
-      fpath+=($HOME/.zfunc)
-    '';
+    initContent = lib.mkMerge [
+      (lib.mkBefore
+        ''
+          # Handle dumb terms
+          [[ $TERM == "dumb" ]] && unsetopt zle && PS1='$ ' && return
+          fpath+=($HOME/.zfunc)
+        '')
+      (lib.mkOrder
+        1000
+        ''
+          ## awscli completion
+          source '${pkgs.awscli}/share/zsh/site-functions/aws_zsh_completer.sh'
 
-    initExtra = ''
-      ## awscli completion
-      source '${pkgs.awscli}/share/zsh/site-functions/aws_zsh_completer.sh'
+          ## prompt
+          # VCS integration setup
+          autoload -Uz vcs_info
+          precmd_vcs_info() { vcs_info }
+          precmd_functions+=( precmd_vcs_info )
+          setopt prompt_subst
+          zstyle ':vcs_info:git:*' formats '%b'
 
-      ## prompt
-      # VCS integration setup
-      autoload -Uz vcs_info
-      precmd_vcs_info() { vcs_info }
-      precmd_functions+=( precmd_vcs_info )
-      setopt prompt_subst
-      zstyle ':vcs_info:git:*' formats '%b'
+          # Enable colors in prompt
+          autoload -U colors && colors
+          zmodload zsh/datetime
 
-      # Enable colors in prompt
-      autoload -U colors && colors
-      zmodload zsh/datetime
+          export VIRTUAL_ENV_DISABLE_PROMPT=1
+          # Based on https://github.com/sindresorhus/pure but with less
+          # functionality
+          # TODO: Pure has a nice timer function that fires if a command runs
+          # for more than 5 seconds, adding the timestamp seems to be a workable
+          # solution but might be nice to add that back in
+          if [[ -n ''${SSH_CONNECTION-}''${SSH_CLIENT-}''${SSH_TTY-} ]]; then
+            export PS1='
+          %{$fg[red]%}%m%{$reset_color%} %{$fg[blue]%}%~%{$reset_color%} %F{242%}%{$vcs_info_msg_0_%}%f%u
+          %{%(?.$fg[magenta].$fg[red])%}❯%{$reset_color%} '
+          elif [[ "$HOSTNAME" == 'toolbox' ]]; then
+            export PS1='
+          %{$fg[blue]%}%~%{$reset_color%} (toolbox) %F{242%}%{$vcs_info_msg_0_%}%f%u
+          %{%(?.$fg[magenta].$fg[red])%}❯%{$reset_color%} '
+          else
+            export PS1='
+          %{$fg[blue]%}%~%{$reset_color%} %F{242%}%{$vcs_info_msg_0_%}%f%u
+          %{%(?.$fg[magenta].$fg[red])%}❯%{$reset_color%} '
+          fi
 
-      export VIRTUAL_ENV_DISABLE_PROMPT=1
-      # Based on https://github.com/sindresorhus/pure but with less
-      # functionality
-      # TODO: Pure has a nice timer function that fires if a command runs
-      # for more than 5 seconds, adding the timestamp seems to be a workable
-      # solution but might be nice to add that back in
-      if [[ -n ''${SSH_CONNECTION-}''${SSH_CLIENT-}''${SSH_TTY-} ]]; then
-        export PS1='
-      %{$fg[red]%}%m%{$reset_color%} %{$fg[blue]%}%~%{$reset_color%} %F{242%}%{$vcs_info_msg_0_%}%f%u
-      %{%(?.$fg[magenta].$fg[red])%}❯%{$reset_color%} '
-      elif [[ "$HOSTNAME" == 'toolbox' ]]; then
-        export PS1='
-      %{$fg[blue]%}%~%{$reset_color%} (toolbox) %F{242%}%{$vcs_info_msg_0_%}%f%u
-      %{%(?.$fg[magenta].$fg[red])%}❯%{$reset_color%} '
-      else
-        export PS1='
-      %{$fg[blue]%}%~%{$reset_color%} %F{242%}%{$vcs_info_msg_0_%}%f%u
-      %{%(?.$fg[magenta].$fg[red])%}❯%{$reset_color%} '
-      fi
+          autoload -z edit-command-line
+          zle -N edit-command-line
+          bindkey "^X^E" edit-command-line
 
-      autoload -z edit-command-line
-      zle -N edit-command-line
-      bindkey "^X^E" edit-command-line
+          # do not highlight pasted text
+          zle_highlight+=(paste:none)
 
-      # do not highlight pasted text
-      zle_highlight+=(paste:none)
-
-      ## mise
-      eval "$(${pkgs.mise}/bin/mise activate zsh)"
-    '';
+          ## mise
+          eval "$(${pkgs.mise}/bin/mise activate zsh)"
+        '')
+    ];
 
     profileExtra = ''
       export PATH="${lib.concatStringsSep ":" config.home.sessionPath}:$PATH''${PATH:+:}"
